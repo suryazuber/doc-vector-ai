8fc0b2a4c5e381bd5e23e27c73b8397587ad93b0
ZS <zuber.s@crestinfosystems.com>
2026-02-11 11:08:09 +0530

    Added change for streamlit version fix

--- rag_utility.py
+++ rag_utility.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-from langchain_community.document_loaders import UnstructuredPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
@@ -25,8 +25,8 @@ llm = ChatGroq(
 
 
 def process_document_to_chroma_db(file_name):
-    # Load the PDF document using UnstructuredPDFLoader
-    loader = UnstructuredPDFLoader(f"{working_dir}/{file_name}")
+    # Load the PDF document using PyPDFLoader
+    loader = PyPDFLoader(f"{working_dir}/{file_name}")
     documents = loader.load()
     # Split the text into chunks for embedding
     text_splitter = RecursiveCharacterTextSplitter(
